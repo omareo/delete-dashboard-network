53c4b484b1855f05f9c63d8679a4a001f1c38be8
Omar Ontiveros <omareo@gmail.com>
2016-06-01 11:07:31 -0700

    formatting

--- delete-dashboard-network.go
+++ delete-dashboard-network.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"os"
-	"net/http"
+	"bufio"
 	"encoding/json"
 	"fmt"
-	"bufio"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 	"strings"
 )
 
 type Network struct {
-	ID 					string `json:"id"`
-	OrganizationID		string `json:"organizationid"`
-	Type 				string `json:"type"`
-	Name				string `json:"name"`
-	TimeZone			string `json:"timeZone"`
-	Tags				string `json:"tags"`
+	ID             string `json:"id"`
+	OrganizationID string `json:"organizationid"`
+	Type           string `json:"type"`
+	Name           string `json:"name"`
+	TimeZone       string `json:"timeZone"`
+	Tags           string `json:"tags"`
 }
 
 func stripchars(str, chr string) string {
@@ -26,7 +26,7 @@ func stripchars(str, chr string) string {
 			return r
 		}
 		return -1
-		}, str)
+	}, str)
 }
 
 func main() {
@@ -40,7 +40,7 @@ func main() {
 	// Fire off http request to get list of Networks and return results to console
 
 	getNetUrl := "https://" + DASHBOARD_API_SHARD_ID + ".meraki.com/api/v0/organizations/" + DASHBOARD_API_ORG_ID + "/networks"
-	getNetworks, err := http.NewRequest("GET", getNetUrl, nil) 
+	getNetworks, err := http.NewRequest("GET", getNetUrl, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -62,70 +62,69 @@ func main() {
 	json.Unmarshal(body, &networks)
 
 	for n := range networks {
-	fmt.Printf(networks[n].Name)
-	fmt.Println()
-    }
+		fmt.Printf(networks[n].Name)
+		fmt.Println()
+	}
 
-    // get user input, select network to drop
-    fmt.Println("Type in the name you would like to remove")
- 
+	// get user input, select network to drop
+	fmt.Println("Type in the name you would like to remove")
 
 	r := bufio.NewReader(os.Stdin)
 	const endOfString = '\n'
 
-    userInputNetworkName, err := r.ReadString(endOfString)
-    if err != nil {
-        log.Fatal(err)
-        os.Exit(1)
-    }
+	userInputNetworkName, err := r.ReadString(endOfString)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
 	fixedUserInputNetworkName := stripchars(userInputNetworkName, "\n")
-	
+
 	for n := range networks {
-   //confirmation of user selection 
-	if fixedUserInputNetworkName == networks[n].Name {
-		fixedUserInputNetworkName := stripchars(userInputNetworkName, "\n")
-		fmt.Println("the following network will be removed:")
-		fmt.Printf(networks[n].Name)
-		fmt.Println()
-	for n := range networks {
-	
+		//confirmation of user selection
 		if fixedUserInputNetworkName == networks[n].Name {
-			fmt.Println("Please confirm with Y")
-		
-		b := bufio.NewReader(os.Stdin)
-		const endOfString = '\n'
+			fixedUserInputNetworkName := stripchars(userInputNetworkName, "\n")
+			fmt.Println("the following network will be removed:")
+			fmt.Printf(networks[n].Name)
+			fmt.Println()
+			for n := range networks {
 
-    		userInputChoice, err := b.ReadString(endOfString)	
-    		fixeduserInputChoice := stripchars(userInputChoice, "\n")
-    	
-    		if err != nil {
-        		log.Fatal(err)
-        		os.Exit(1)
-    		}
-   	 	
-   	 		if fixeduserInputChoice == "Y" {
-				//build url
-				deletNetUrl := "https://" + DASHBOARD_API_SHARD_ID + ".meraki.com/api/v0/networks/" + networks[n].ID
+				if fixedUserInputNetworkName == networks[n].Name {
+					fmt.Println("Please confirm with Y")
 
-				// Fire off http DELETE 
-				deleteNetwork, err := http.NewRequest("DELETE", deletNetUrl, nil)
-				if err != nil {
-					log.Fatal(err)
+					b := bufio.NewReader(os.Stdin)
+					const endOfString = '\n'
+
+					userInputChoice, err := b.ReadString(endOfString)
+					fixeduserInputChoice := stripchars(userInputChoice, "\n")
+
+					if err != nil {
+						log.Fatal(err)
+						os.Exit(1)
+					}
+
+					if fixeduserInputChoice == "Y" {
+						//build url
+						deletNetUrl := "https://" + DASHBOARD_API_SHARD_ID + ".meraki.com/api/v0/networks/" + networks[n].ID
+
+						// Fire off http DELETE
+						deleteNetwork, err := http.NewRequest("DELETE", deletNetUrl, nil)
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						client := http.Client{}
+						deleteNetwork.Header.Add("X-Cisco-Meraki-API-Key", DASHBOARD_API_KEY)
+						requestResult, err := client.Do(deleteNetwork)
+						if err != nil {
+							log.Fatal(err)
+						}
+						requestResult.Body.Close()
+						fmt.Println("Network has been deleted.")
+
+					}
 				}
-
-				client := http.Client{}
-				deleteNetwork.Header.Add("X-Cisco-Meraki-API-Key", DASHBOARD_API_KEY)
-				requestResult, err := client.Do(deleteNetwork)
-				if err != nil {
-					log.Fatal(err)
-				}
-				requestResult.Body.Close()
-				fmt.Println("Network has been deleted.")
-
-			}		
-		  }
- 		}	
-      }
-    }
+			}
+		}
+	}
 }
